25cc761b76d9b951ef3bc421bfa72bee47ef5968
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-01-27 08:58:03 +0800

    Addded design on the SplashView.dart  and Added Route.dart 👌

---
+++ lib/src/view/SplashView.dart
@@ -0,0 +1,97 @@
+// views/splash_screen.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../view_model/SplashViewModel.dart';
+
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final splashViewModel = Provider.of<SplashViewModel>(context, listen: true);
+
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 66, 66, 66),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: const DecorationImage(
+                      image: AssetImage('assets/images/logo2.jpg'),
+                      fit: BoxFit.fill,
+                    ),
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 2,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3), // Changes position of shadow
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'MINANDAYA ',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontFamily: 'OpenSans',
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'MOBILE ',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontFamily: 'LeagueSpartan',
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'TRANSLATOR',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontFamily: 'OpenSans',
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              splashViewModel.isLoading
+                  ? const SizedBox(
+                width: 80,
+                height: 80,
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              )
+                  : const Text(''),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
