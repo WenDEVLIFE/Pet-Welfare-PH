31726885dca7b45bbe0258386c8a18642225d839
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-01-27 09:00:18 +0800

    Added functions on the Route.dart 👌

---
+++ lib/src/helpers/Route.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+import '../view/SplashView.dart';
+
+class AppRoutes {
+  static const String splashScreen1 = '/splashscreen1';
+
+  static Map<String, WidgetBuilder> routes = {
+    splashScreen1: (context) => Placeholder(), // Replace with actual screen
+  };
+}
