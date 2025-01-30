c88b143b243f5f0a0f46ff2f2b853d2ec22ca2c7
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-01-30 08:58:25 +0800

    Added selectview on the Route.dart 😊

--- lib/src/helpers/Route.dart
+++ lib/src/helpers/Route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:pet_welfrare_ph/src/view/LoginView.dart';
+import 'package:pet_welfrare_ph/src/view/SelectView.dart';
 
 import '../view/SplashView.dart';
 import '../view/LoadingView.dart';
@@ -8,11 +9,13 @@ class AppRoutes {
   static const String loadingScreen = '/loadingscreen';
   static const String splashscreen = '/splashscreen';
   static const String loginScreen = '/loginScreen';
+  static const String selectScreen = '/selectScreen';
 
   // Assign routes to the screens
   static Map<String, WidgetBuilder> routes = {
     loadingScreen: (context) => LoadingView(), // Replace with actual screen
     splashscreen: (context) => SplashView2(), // Replace with actual screen
     loginScreen: (context) => const Loginview(), // Replace with actual screen
+    selectScreen: (context) => const SelectView(), // Replace with actual screen
   };
 }
