80451e1422034c82e51cac1f716439a756e8a5a7
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-05 23:19:23 +0800

    Added TermsAndConditionView.dart and PrivacyView.dart 👌

---
+++ lib/src/view/TermsAndConditionView.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/AppColors.dart';
+
+class TermsAndConditionView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Terms and Conditions',
+          style: TextStyle(
+            fontFamily: 'SmoochSans',
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),),
+        backgroundColor: AppColors.orange,
+      ),
+      body: Center(
+        child: Text('Terms and Conditions'),
+      ),
+    );
+  }
+}
