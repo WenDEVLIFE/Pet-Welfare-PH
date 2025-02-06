b45fb5e494508b08b0c1aaf392008ee223d503d5
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-06 14:13:18 +0800

    Done designing the ChangePasswordViewModel.dart and its UI ChangePasswordViewModel.dart 👌

--- lib/src/view/ChangePasswordView.dart
+++ lib/src/view/ChangePasswordView.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pet_welfrare_ph/src/view_model/ChangePasswordViewModel.dart';
+import 'package:provider/provider.dart';
+
+import '../utils/AppColors.dart';
 
 class ChangePasswordView extends StatefulWidget {
   const ChangePasswordView({Key? key}) : super(key: key);
@@ -8,10 +13,143 @@ class ChangePasswordView extends StatefulWidget {
 }
 
 class ChangePasswordState extends State <ChangePasswordView> {
+
+  late ChangePasswordViewModel viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    viewModel = Provider.of<ChangePasswordViewModel>(context, listen: false);
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
     // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Edit Password',
+          style: TextStyle(
+            fontFamily: 'SmoochSans',
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: AppColors.orange,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const Text('OLD PASSWORD', style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w900,
+                fontFamily: 'SmoochSans',
+                color: Colors.black)),
+            SizedBox(height: screenHeight * 0.01),
+            Consumer<ChangePasswordViewModel>(
+              builder: (context, changeViewModel, child) {
+                return TextField(
+                  controller: viewModel.oldPasswordController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppColors.gray,
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        changeViewModel.obscureText1 ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        changeViewModel.togglePasswordVisibility1();
+                      },
+                    ),
+                    hintText: 'Enter old your password',
+                    hintStyle: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  obscureText: changeViewModel.obscureText1,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'SmoochSans',
+                    fontWeight: FontWeight.w600,
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            const Text('NEW PASSWORD', style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w900,
+                fontFamily: 'SmoochSans',
+                color: Colors.black)),
+            SizedBox(height: screenHeight * 0.01),
+            Consumer<ChangePasswordViewModel>(
+              builder: (context, changeViewModel, child) {
+                return TextField(
+                  controller: viewModel.oldPasswordController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppColors.gray,
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        changeViewModel.obscureText2 ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        changeViewModel.togglePasswordVisibility2();
+                      },
+                    ),
+                    hintText: 'Enter new your password',
+                    hintStyle: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  obscureText: changeViewModel.obscureText2,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'SmoochSans',
+                    fontWeight: FontWeight.w600,
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            Center(
+              child: Container(
+                width: screenWidth * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(color: Colors.transparent),
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                  ),
+                  child: const Text('Update Password',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'SmoochSans',
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
   
 }
--- lib/src/view_model/ChangePasswordViewModel.dart
+++ lib/src/view_model/ChangePasswordViewModel.dart
@@ -5,5 +5,20 @@ class ChangePasswordViewModel extends ChangeNotifier {
   final TextEditingController oldPasswordController = TextEditingController();
   final TextEditingController newPasswordController = TextEditingController();
 
+  bool obscureText1 = true;
+  bool obscureText2 = true;
+
+  bool get isobscure1 => obscureText1;
+  bool get isobscure2 => obscureText2;
+
+  void togglePasswordVisibility1() {
+    obscureText1 = !obscureText1;
+    notifyListeners();
+  }
+
+  void togglePasswordVisibility2() {
+    obscureText2 = !obscureText2;
+    notifyListeners();
+  }
 
 }
