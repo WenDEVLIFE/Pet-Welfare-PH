8dc64218bf4e0fd2bcacceb50d2e7022f9313c3e
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-06 10:31:39 +0800

    Added ChangePasswordView.dart and added minor text to RegisterViewModel.dart 👌

---
+++ lib/src/view/ChangePasswordView.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class ChangePasswordView extends StatefulWidget {
+  const ChangePasswordView({Key? key}) : super(key: key);
+
+  @override
+  ChangePasswordState createState() => ChangePasswordState();
+}
+
+class ChangePasswordState extends State <ChangePasswordView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
