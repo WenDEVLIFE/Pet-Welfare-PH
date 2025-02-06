96cddb17e124743456acd934dd9d10c0a4a25231
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-06 13:52:18 +0800

    Added ChangePasswordViewModel.dart 👌

---
+++ lib/src/view_model/ChangePasswordViewModel.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ChangePasswordViewModel extends ChangeNotifier {
+
+  final TextEditingController oldPasswordController = TextEditingController();
+  final TextEditingController newPasswordController = TextEditingController();
+
+
+}
