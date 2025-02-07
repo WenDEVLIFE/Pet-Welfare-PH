040229cf73ac702b95037a5b98340892cf1a1d25
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-07 10:23:55 +0800

    Added the NavigationDrawer.dart and connect it to the DashboardView.dart 👌

---
+++ lib/src/components/NavigationDrawer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NavigationDrawer extends StatelessWidget {
+  final List<Widget> children;
+
+  const NavigationDrawer({Key? key, required this.children}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: children,
+      ),
+    );
+  }
+}
--- lib/src/view/DashboardView.dart
+++ lib/src/view/DashboardView.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({Key? key}) : super(key: key);
@@ -10,8 +10,60 @@ class DashboardView extends StatefulWidget {
 class DashboardViewState extends State<DashboardView> {
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      child: Center(
+    return Scaffold(
+      drawer: NavigationDrawer(
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'Navigation Menu',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Dashboard'),
+            onTap: () {
+              Navigator.pop(context);
+              // Navigate to Home
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Verified Users'),
+            onTap: () {
+              Navigator.pop(context);
+              // Navigate to Settings
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.contacts),
+            title: Text('Pending User Verification'),
+            onTap: () {
+              Navigator.pop(context);
+              // Navigate to Contacts
+            },
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        title: const Text(
+          'Dashboard',
+          style: TextStyle(
+            fontFamily: 'SmoochSans',
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(
         child: Text('Dashboard View'),
       ),
     );
