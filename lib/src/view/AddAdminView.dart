9494dab69a50a743dda1b5246a1ca502f65b584c
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-08 08:22:53 +0800

    Added AddAdminView.dart and AddAdminViewModel.dart 👌

---
+++ lib/src/view/AddAdminView.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class AddAdminView extends StatefulWidget {
+  const AddAdminView({Key? key}) : super(key: key);
+
+  @override
+  AddAdminViewState createState() => AddAdminViewState();
+}
+
+class AddAdminViewState extends State<AddAdminView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
