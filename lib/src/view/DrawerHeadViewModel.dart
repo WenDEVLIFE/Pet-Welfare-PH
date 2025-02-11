797470f6b43e65d95fcae8ced1d49b86d33f2154
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-11 09:31:59 +0800

    Added DrawerHeadViewModel.dart

---
+++ lib/src/view/DrawerHeadViewModel.dart
@@ -0,0 +1,18 @@
+import 'dart:core';
+
+import 'package:flutter/cupertino.dart';
+
+class DrawerHeadViewModel extends ChangeNotifier {
+
+  final String name = "name";
+
+  final String role = "role";
+
+  final String profileImage = "profileImage";
+
+  void LoadData() {
+
+
+  }
+
+}
