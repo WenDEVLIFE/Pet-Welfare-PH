a06b078c8d8654c8ea388c262231dbbb69a435ce
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-17 14:08:20 +0800

    Added ToastComponent.dart 👌

---
+++ lib/src/utils/ToastComponent.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ToastComponent {
+  Future<Future<bool?>> showMessage(Color color, String message, BuildContext context) async {
+    return Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: color,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
+}
