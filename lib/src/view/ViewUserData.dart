1aa9e02ff3f788fdec23a9a28dee686d962c595f
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-18 11:46:54 +0800

    Added UserDataViewModel.dart and ViewUserData.dart

---
+++ lib/src/view/ViewUserData.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class ViewUserData extends StatefulWidget {
+  const ViewUserData({Key? key}) : super(key: key);
+
+  @override
+  _ViewUserDataState createState() => _ViewUserDataState();
+}
+
+// TODO : Implement the state of the ViewUserData and the UI of the ViewUserData
+class _ViewUserDataState extends State<ViewUserData>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/src/view_model/UserDataViewModel.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class UserDataViewModel extends ChangeNotifier {
+
+  // TODO implement the ViewModel
+
+}
