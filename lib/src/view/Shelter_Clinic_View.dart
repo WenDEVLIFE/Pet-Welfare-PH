2ee2092e32790f5cf25ccebc76119489a9254cc0
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-22 08:48:18 +0800

    Added Shelter_Clinic_View.dart and ShelterClinicViewModel.dart 👌

---
+++ lib/src/view/Shelter_Clinic_View.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class Shelter_Clinic_View extends StatefulWidget {
+  const Shelter_Clinic_View({Key? key}) : super(key: key);
+
+  @override
+  Shelter_Clinic_ViewState createState() => Shelter_Clinic_ViewState();
+}
+
+class Shelter_Clinic_ViewState  extends State<Shelter_Clinic_View> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("View for Shelter and Clinic"),
+    );
+  }
+}
---
+++ lib/src/view_model/ShelterClinicViewModel.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+class ShelterClinicViewModel extends ChangeNotifier {
+  // Add your code here
+}
