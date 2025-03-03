b60456aa6bbc9157cefa2ebb4147d2f8ac2e60e5
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-03 20:49:57 +0800

    Added MessageRepository.dart and added minor functions on the MessageViewModel.dart 👌

---
+++ lib/src/respository/MessageRepository.dart
@@ -0,0 +1,9 @@
+abstract class MessageRepository {
+
+
+}
+
+class MessageRepositoryImpl implements MessageRepository {
+
+}
+
--- lib/src/view_model/MessageViewModel.dart
+++ lib/src/view_model/MessageViewModel.dart
@@ -2,4 +2,10 @@ import 'package:flutter/cupertino.dart';
 
 class MessageViewModel  extends ChangeNotifier {
 
+  final TextEditingController messageController = TextEditingController();
+
+  final String ImageReceiver ='';
+
+
+
 }
