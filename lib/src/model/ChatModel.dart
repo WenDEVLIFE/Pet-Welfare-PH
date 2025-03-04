49d2170ff0cc1e492ed0e430cefd0febcbc21e9a
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-04 10:41:39 +0800

    Added ChatModel.dart 👌

---
+++ lib/src/model/ChatModel.dart
@@ -0,0 +1,14 @@
+class ChatModel{
+  final String id;
+  final String name;
+  final String profilepath;
+  final String lastMessage;
+
+
+  ChatModel({
+    required this.id,
+    required this.name,
+    required this.profilepath,
+    required this.lastMessage,
+  });
+}
