e26319bc9969d3dfd9025597d4b06ea60020f1b2
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-07 09:19:49 +0800

    Done designing the ViewImage.dart 👌

--- lib/src/view/ViewImage.dart
+++ lib/src/view/ViewImage.dart
@@ -1,1 +1,32 @@
-// TODO : Added View Image Screen
+import 'package:flutter/material.dart';
+
+class ViewImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var imageData = (ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?)!;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('View Image' , style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.close , color: Colors.white),
+            onPressed: () {
+              // Implement share functionality here
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          child: Image.network(
+            imageData['imagePath']!,
+            fit: BoxFit.contain, // Ensures the image fits well
+          ),
+        ),
+      ),
+    );
+  }
+}
