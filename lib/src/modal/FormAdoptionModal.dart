e257a308d173381768b919bbbd1f57bb259ffc0c
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-22 10:59:23 +0800

    Added FormAdoptionModal.dart

---
+++ lib/src/modal/FormAdoptionModal.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FormAdoptionModal extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context){
+
+    // TODO: Create a form for adoption
+    return const Scaffold(
+
+    );
+  }
+}
