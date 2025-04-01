ebd4f04d70efd46254c077b72f6a9f7b5650db07
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-04-01 16:03:51 +0800

    Added a function where it will store the donations from the database 👌

--- lib/src/respository/DonationRepository.dart
+++ lib/src/respository/DonationRepository.dart
@@ -1,7 +1,81 @@
-abstract class DonationRepository {
+import 'dart:io';
+import 'dart:typed_data'; // Correct import for Uint8List
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:pet_welfrare_ph/src/utils/SessionManager.dart';
+import 'package:pet_welfrare_ph/src/utils/ToastComponent.dart';
+
+abstract class DonationRepository {
+  Future <void> submitDonation(Map<String, dynamic> donation);
 }
 
 class DonationRepositoryImpl implements DonationRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final SessionManager _sessionManager = SessionManager();
 
+  // Add the donation to the database
+  @override
+  Future<void> submitDonation(Map<String, dynamic> donation) async {
+    User user = _auth.currentUser!;
+    String id = user.uid;
+    String postID = donation['postId'];
+    String amount = donation['amount'];
+    String date = donation['date'];
+    String time = donation['time'];
+
+    final userdata = await _sessionManager.getUserInfo();
+    String name = userdata?['name'];
+
+    File imageFile = File(donation['transactionPath']!);
+    if (await imageFile.exists()) {
+      Uint8List messageBytes = await imageFile.readAsBytes();
+
+      String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+      Reference storageRef = _storage.ref().child('PostFolder/$postID/$fileName.jpg');
+      UploadTask uploadTask = storageRef.putData(messageBytes);
+      TaskSnapshot taskSnapshot = await uploadTask;
+      String downloadUrl = await taskSnapshot.ref.getDownloadURL();
+
+      try {
+        DocumentSnapshot documentSnapshot = await _firestore.collection('PostCollection')
+            .doc(postID)
+            .get();
+
+        if (documentSnapshot.exists) {
+          String postOwnerID = documentSnapshot['PostOwnerID'];
+          await _firestore.collection("PostCollection").doc(postID).collection("DonationCollection").add({
+            'Amount': amount,
+            'Date': date,
+            'Time': time,
+            'TransactionFile': '$fileName.jpg',
+            'TransactionPath': downloadUrl,
+            'UserID': id,
+          });
+
+          DocumentReference notificationRef = _firestore.collection('NotificationCollection').doc();
+          await notificationRef.set({
+            'notificationID': notificationRef.id,
+            'userID': postOwnerID,
+            'content': 'You have received a donation from $name',
+            'timestamp': FieldValue.serverTimestamp(),
+            'category': 'Donation',
+            'isRead': false,
+          });
+
+          ToastComponent().showMessage(Colors.green, 'Donation submitted successfully');
+        } else{
+          ToastComponent().showMessage(Colors.red, 'Donation failed');
+        }
+
+
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
 }
--- lib/src/view_model/DonateViewModel.dart
+++ lib/src/view_model/DonateViewModel.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:pet_welfrare_ph/src/respository/DonationRepository.dart';
+import 'package:sn_progress_dialog/progress_dialog.dart';
 
 class DonateViewModel extends ChangeNotifier {
 
@@ -8,6 +10,8 @@ class DonateViewModel extends ChangeNotifier {
   TextEditingController dateController = TextEditingController();
   TextEditingController timeController = TextEditingController();
   String transactionPath='';
+
+  final DonationRepository _donationRepository = DonationRepositoryImpl();
 
   // Select a transaction date
   void selectDate(BuildContext context) async {
@@ -50,7 +54,24 @@ class DonateViewModel extends ChangeNotifier {
     String dateValue = dateController.text;
     String timeValue = timeController.text;
     if (amountValue.isNotEmpty && dateValue.isNotEmpty && timeValue.isNotEmpty) {
-
+      ProgressDialog pd = ProgressDialog(context: context);
+      pd.show(max: 100, msg: 'Submitting...');
+       try{
+         var donation = {
+           'amount': amountValue,
+           'date': dateValue,
+           'time': timeValue,
+           'postId': postId,
+           'transactionPath': transactionPath,
+         };
+          _donationRepository.submitDonation(donation);
+        Navigator.of(context).pop();
+       } catch(e){
+         print(e);
+       }
+       finally{
+         pd.close();
+       }
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
