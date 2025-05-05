7b3c8d4eaf52aa5a633e9fad1e9c2572d609ac3e
Frouen M. Medina Jr. <frouenmedinajr@gmail.com>
2025-05-05 20:13:52 +0800

    Added AdoptionModel.dart and Added minor ui to the ViewAdoptionModal.dart

---
+++ lib/src/modal/ViewAdoptionModal.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:pet_welfrare_ph/src/view_model/ApplyAdoptionViewModel.dart';
+
+class ViewAdoptionModal extends StatelessWidget{
+
+  final String postId;
+  const ViewAdoptionModal({Key? key, required this.postId}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'List of Adoptions',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: StreamBuilder<List<ApplyAdoptionViewModel>>(
+              stream: postViewModel.getPetAdoption(widget.postId),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+
+                if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No adoptions found.'));
+                }
+
+                var adoption = snapshot.data!;
+
+                return ListView.builder(
+                  itemCount: adoption.length,
+                  itemBuilder: (context, index) {
+                    final adoptionDetails = adoption[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  adoptionDetails.Name,
+                                  style: const TextStyle(
+                                    fontFamily: 'SmoochSans',
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  adoptionDetails.commentText,
+                                  style: const TextStyle(
+                                    fontFamily: 'SmoochSans',
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  adoptionDetails.formattedTimestamp,
+                                  style: const TextStyle(
+                                    fontFamily: 'SmoochSans',
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.delete, color: Colors.red),
+                                onPressed: () async {
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+}
---
+++ lib/src/model/AdoptionModel.dart
@@ -0,0 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AdoptionModal {
+
+  final String adoptionID;
+
+  final String adoptionName;
+
+  final String facebookUsername;
+
+  final String phoneNum;
+
+  final String email;
+
+  final String address;
+
+  final String adoptionType;
+
+  final String region;
+
+  final String province;
+
+  final String city;
+
+  final String barangay;
+
+
+
+  AdoptionModal({
+    required this.adoptionID,
+    required this.adoptionName,
+    required this.facebookUsername,
+    required this.phoneNum,
+    required this.email,
+    required this.address,
+    required this.adoptionType,
+    required this.region,
+    required this.province,
+    required this.city,
+    required this.barangay,
+  });
+
+  factory AdoptionModal.fromDocumentSnapshot(DocumentSnapshot doc){
+    return AdoptionModal(
+      adoptionID: doc.id ?? '',
+      adoptionName: doc['Name'] ?? '',
+      facebookUsername: doc['FacebookUsername'] ?? '',
+      phoneNum: doc['Phone'] ?? '',
+      email: doc['Email'] ?? '',
+      address: doc['Address'] ?? '',
+      adoptionType: doc['AdoptionType'] ?? '',
+      region: doc['Region'] ?? '',
+      province: doc['Province'] ?? '',
+      city: doc['City'] ?? '',
+      barangay: doc['Barangay'] ?? '',
+    );
+  }
+
+
+}
