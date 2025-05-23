3a1582df714e4a4f972501e8ee5d75eb262452a0
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-05-23 16:24:29 +0800

    Added Design on to the ReportView.dart 👌

--- lib/src/view/admindirectory/ReportView.dart
+++ lib/src/view/admindirectory/ReportView.dart
@@ -1,16 +1,234 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pet_welfrare_ph/src/utils/AppColors.dart';
+import '../../widgets/DrawerHeaderWidget.dart';
+import '../../widgets/LogoutDialog.dart';
+import '../../model/SubcriptionModel.dart';
+import '../../utils/Route.dart';
+import '../../utils/SessionManager.dart';
+import 'package:provider/provider.dart';
+import '../../view_model/SubcriptionViewModel.dart';
+import '../../widgets/SearchTextField.dart';
 
 class ReportView extends StatefulWidget {
   const ReportView({Key? key}) : super(key: key);
 
   @override
-  State<ReportView> createState() => _ReportViewState ();  
+  _ReportState createState() => _ReportState();
 }
 
-class _ReportViewState extends State<ReportView>{
+class _ReportState extends State<ReportView> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      drawer: NavigationDrawer(
+        children: [
+          const DrawerHeaderWidget(),
+          _buildDrawerItem(Icons.dashboard, 'Dashboard', () {
+            Navigator.pop(context);
+            Navigator.pushReplacementNamed(context, AppRoutes.dashboard);
+          }),
+          _buildDrawerItem(Icons.verified_user_rounded, 'Users', () {
+            Navigator.pop(context);
+            Navigator.pushReplacementNamed(context, AppRoutes.userView);
+          }),
+          _buildDrawerItem(Icons.home, 'Home', () {
+            Navigator.pop(context);
+            Navigator.pushReplacementNamed(context, AppRoutes.homescreen);
+          }),
+          _buildDrawerItem(Icons.attach_money, 'Subscriptions', () {
+            Navigator.pop(context);
+            Navigator.pushReplacementNamed(context, AppRoutes.subscription);
+          }),
+          _buildDrawerItem(Icons.holiday_village_outlined, 'All Business', () {
+            Navigator.pop(context);
+            Navigator.pushReplacementNamed(context, AppRoutes.adminViewEstablishment);
+          }),
+          _buildDrawerItem(Icons.report_gmailerrorred_sharp, 'Reports', () {
+            Navigator.pop(context);
+            Navigator.pushReplacementNamed(context, AppRoutes.reportView);
+          }),
+          _buildDrawerItem(Icons.info, 'About us', () {
+            Navigator.pop(context);
+            Navigator.pushNamed(context, AppRoutes.about);
+            // Navigate to Privacy Policy
+          }),
+          _buildDrawerItem(Icons.check, 'Terms and Condition', () {
+            Navigator.pop(context);
+            Navigator.pushNamed(context, AppRoutes.termsAndConditions);
+          }),
+          _buildDrawerItem(Icons.privacy_tip_outlined, 'Privacy Policy', () {
+            Navigator.pop(context);
+            Navigator.pushNamed(context, AppRoutes.privacyPolicy);
+          }),
+          _buildDrawerItem(Icons.logout, 'Logout', () {
+            Navigator.pop(context);
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return LogoutDialog(
+                  onLogout: () async {
+                    await SessionManager().clearUserInfo();
+                    Navigator.pushReplacementNamed(context, AppRoutes.loginScreen);
+                    print('User logged out');
+                  },
+                );
+              },
+            );
+          }),
+        ],
+      ),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ),
+        title: const Text(
+          'List of Reports',
+          style: TextStyle(
+            color: AppColors.white,
+            fontSize: 20,
+            fontFamily: 'SmoochSans',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        backgroundColor: AppColors.orange,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: screenHeight * 0.005),
+           /*CustomSearchTextField(
+          controller: _subscriptionViewModel.searchController,
+            screenHeight: screenHeight,
+            hintText: 'Search a subscription....',
+            fontSize: 16,
+            keyboardType: TextInputType.text,
+            onChanged: (searchText) {
+             // _subscriptionViewModel.filterSubscriptions(searchText);
+            },
+          ),
+          SizedBox(height: screenHeight * 0.005),
+           Expanded(
+            child: FutureBuilder<List<SubscriptionModel>>(
+              future: _subscriptionViewModel.subscriptionsStream.first,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return const Center(child: Text('No subscriptions found'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No subscriptions available'));
+                } else {
+                  final subscriptions = snapshot.data!;
+                  _subscriptionViewModel.setSubscriptions(subscriptions);
+                  return Consumer<SubscriptionViewModel>(
+                    builder: (context, viewModel, child) {
+                      return Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: ListView.builder(
+                          itemCount: viewModel.subscriptionsdata.length,
+                          itemBuilder: (context, index) {
+                            SubscriptionModel subscription = viewModel.subscriptionsdata[index];
+                            return Card(
+                              color: AppColors.orange,
+                              child: ListTile(
+                                title: Text(subscription.subscriptionName,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w800,
+                                    fontFamily: 'SmoochSans',
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Duration: ${subscription.subscriptionDuration} days',
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w800,
+                                        fontFamily: 'SmoochSans',
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    Text('Amount: ₱ ${subscription.subscriptionAmount}',
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w800,
+                                        fontFamily: 'SmoochSans',
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(Icons.edit, color: Colors.white),
+                                      onPressed: () => _editSubscription(context, subscription, subscription.uid),
+                                    ),
+                                    IconButton(
+                                      icon: const Icon(Icons.delete, color: Colors.white),
+                                      onPressed: () {
+                                        viewModel.deleteSubscription(context, subscription.uid);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ), */
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FloatingActionButton(
+                onPressed: () {
+                  Provider.of<SubscriptionViewModel>(context, listen: false).addSubscriptionRoute(context);
+                },
+                backgroundColor: AppColors.orange,
+                child: const Icon(Icons.add, color: AppColors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDrawerItem(IconData icon, String title, VoidCallback onTap) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: AppColors.black,
+          fontFamily: 'SmoochSans',
+          fontWeight: FontWeight.w600,
+          fontSize: 20,
+        ),
+      ),
+      onTap: onTap,
+    );
   }
 }
