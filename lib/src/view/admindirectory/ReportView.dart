b4dea427734e811e2e74b8b5e65c2f953c703197
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-05-23 09:13:11 +0800

    Added ReportView.dart 👌

---
+++ lib/src/view/admindirectory/ReportView.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class ReportView extends StatefulWidget {
+  const ReportView({Key? key}) : super(key: key);
+
+  @override
+  State<ReportView> createState() => _ReportViewState ();  
+}
+
+class _ReportViewState extends State<ReportView>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
